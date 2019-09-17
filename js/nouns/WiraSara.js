573b773aefa8b5c03a408ed1ca4c798e0aa81deb
CSara23 <51099507+CSara23@users.noreply.github.com>
2019-09-17 14:10:30 -0400

    Create WiraSara.js

---
+++ js/nouns/WiraSara.js
@@ -0,0 +1,18 @@
+nouns = nouns.concat([
+    'koala',
+    'joaquin phoenix',
+    'raccoon',
+    'blueberry',
+    'pappardelle',
+    'periwinkle blue',
+    'peacock',
+    'vodka soda',
+    'mississipi mud',
+    'redhead',
+    'Giza pyramids',
+    'Virgin Islands',
+    'Belle',
+    'Metropolitan Museum of Art',
+    'California sushi roll'
+
+]);
