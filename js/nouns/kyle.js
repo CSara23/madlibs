404017eba932ba8b217cd83fd47c6f783cc24296
CSara23 <51099507+CSara23@users.noreply.github.com>
2019-09-17 14:11:21 -0400

    Update kyle.js

--- js/nouns/kyle.js
+++ js/nouns/kyle.js
@@ -1,21 +1,6 @@
 nouns = nouns.concat([
     'man',
     'bear',
-    'pig',
-    'koala',
-    'joaquin phoenix',
-    'raccoon',
-    'blueberry',
-    'pappardelle',
-    'periwinkle blue',
-    'peacock',
-    'vodka soda',
-    'mississipi mud',
-    'redhead',
-    'Giza pyramids',
-    'Virgin Islands',
-    'Belle',
-    'Metropolitan Museum of Art',
-    'California sushi roll'
+    'pig'
 
 ]);
